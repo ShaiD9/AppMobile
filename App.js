0d0edfa56421a7405d62ab66d1cb390e68a0cd65
ShaiD9 <fffdiamant.orange@gmail.com>
2024-02-19 14:32:35 +0100

    appellation du composant connexion

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import { auto } from 'eol';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import Connexion from './pages/connexion.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <Text style={styles.title}>FDR_Mobile - Connexion</Text>
-        <Text style={styles.label}>Nom d'utilisateur :</Text>
-        <TextInput style={styles.input} placeholder="" />
-        <Text style={styles.label}>Mot de passe :</Text>
-        <TextInput style={styles.input} placeholder="" secureTextEntry={true} />
-        <Button color='#3333ff' title="Connexion" onPress={() => {}} />
-      </View>
+      <Connexion />
     </View>
   );
 }
@@ -23,33 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  card: {
-    backgroundColor: '#f0f0f0',
-    padding: 20,
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-  title: {
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    fontSize: 24,
-  },
-  label: {
-    fontSize: 16,
   },
 });
---
+++ pages/connexion.js
@@ -0,0 +1,56 @@
+import { auto } from 'eol';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function Connexion() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.title}>FDR_Mobile - Connexion</Text>
+        <Text style={styles.label}>Nom d'utilisateur :</Text>
+        <TextInput style={styles.input} placeholder="" />
+        <Text style={styles.label}>Mot de passe :</Text>
+        <TextInput style={styles.input} placeholder="" secureTextEntry={true} />
+        <Button color='#3333ff' title="Connexion" onPress={() => {}} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    backgroundColor: '#f0f0f0',
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  title: {
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    fontSize: 24,
+  },
+  label: {
+    fontSize: 16,
+  },
+});
